f15c07f94632681743eb96fb8e4f6ae2a9fb8e6e
Riatre Foo <foo@riat.re>
2021-02-06 05:54:06 +0800

    yolo checksum

--- attach.sh
+++ attach.sh
@@ -5,3 +5,6 @@
 tc qdisc add dev "$1" clsact
 tc filter add dev "$1" egress bpf direct-action obj 1919.o section egress
 tc filter add dev "$1" ingress bpf direct-action obj 1919.o section ingress
+
+LOWER_IF="`(basename $(realpath $(ls -d /sys/class/net/$1/lower_*)) || echo $1)2>/dev/null`"
+ethtool -K "$LOWER_IF" tx off || true
--- detach.sh
+++ detach.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 
 tc qdisc del dev "$1" clsact
+LOWER_IF="`(basename $(realpath $(ls -d /sys/class/net/$1/lower_*)) || echo $1)2>/dev/null`"
+ethtool -K "$LOWER_IF" tx on || true
